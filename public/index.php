000ceae890d2f7bdf3b639f5ab863a7743152e24
Robert Cuadra <robert@prgmtk.com>
2012-10-16 11:56:24 -0700

    twiml!

--- public/index.php
+++ public/index.php
@@ -7,4 +7,15 @@ $app->get('/', function () {
     echo "hi!";
 });
 
+$app->map('/twiml', function () use ($app) {
+	$res = $app->response()->header('Content-Type', 'application/xml');
+    echo "
+<response>
+<say>You are being enqueued now.</say>
+<enqueue>radio-callin-queue</enqueue>
+</response>
+		";
+})->via('GET', 'POST');
+
+
 $app->run();
